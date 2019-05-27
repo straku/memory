526fbc2b5c8e48445c853a55c5b0261789943f33
Łukasz Strączyński <lukasz.straczynski@grandparade.co.uk>
2019-05-27 22:32:35 +0200

    3C

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import shuffle from 'lodash.shuffle';
 
 import './index.css';
 
@@ -16,32 +17,74 @@ const faces = [
 ];
 
 class Card extends React.Component {
-  state = {
-    flipped: false,
-  };
-
-  handleClick = () => {
-    this.setState({
-      flipped: !this.state.flipped,
-    });
-  };
-
   render() {
+    const { card, state, onFlip } = this.props;
     return (
       <div
-        className={`card ${this.state.flipped ? 'flipped' : ''}`}
-        onClick={this.handleClick}
+        className={`card ${state === 'face-up' ? 'flipped' : ''}`}
+        onClick={() => onFlip(card)}
       >
         <div className="face">♦️</div>
-        <div className="face">{this.props.face}</div>
+        <div className="face">{card.face}</div>
       </div>
     );
   }
 }
 
-ReactDOM.render(
-  <div className="container">
-    <Card face="🦑" />
-  </div>,
-  document.getElementById('root')
-);
+// possible card states:
+// - face-down
+// - face-up
+
+function getCards(length) {
+  const possibleFaces = shuffle(faces).slice(0, length / 2);
+  const allCards = shuffle([...possibleFaces, ...possibleFaces]);
+  return allCards.map((face, i) => ({
+    id: i,
+    face: face,
+  }));
+}
+
+class App extends React.Component {
+  static defaultProps = {
+    size: 4,
+  };
+
+  constructor(props) {
+    super(props);
+    const cards = getCards(props.size);
+    this.state = {
+      cards: cards,
+      cardsState: Array(props.size).fill('face-down'),
+    };
+  }
+
+  handleFlip = card => {
+    const newCardsState = [...this.state.cardsState];
+
+    newCardsState[card.id] = 'face-up';
+
+    this.setState({
+      cardsState: newCardsState,
+    });
+  };
+
+  render() {
+    const { cards, cardsState } = this.state;
+    return (
+      <div className="container">
+        <div className="board">
+          {cards.map(card => (
+            <Card
+              key={card.id}
+              card={card}
+              state={cardsState[card.id]}
+              onFlip={this.handleFlip}
+            />
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
