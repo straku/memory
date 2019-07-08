7dbfff48ba330426551ab62be984693b9405e763
Łukasz Strączyński <l.straczynski@hotmail.com>
2019-07-09 00:13:58 +0200

    Add emoji faces array

--- src/index.js
+++ src/index.js
@@ -3,4 +3,16 @@ import ReactDOM from 'react-dom';
 
 import './index.css';
 
+// prettier-ignore
+const faces = [
+  '🐶', '🐱', '🐭', '🐹',
+  '🐰', '🦊', '🐻', '🐼',
+  '🐨', '🐯', '🦁', '🐮',
+  '🐷', '🐸', '🐵', '🐔',
+  '🦑', '🦖', '🐧', '🦆',
+  '🦅', '🦉', '🦇', '🐺',
+  '🐗', '🐴', '🦄', '🐝',
+  '🐛', '🦋', '🐌', '🐟',
+];
+
 ReactDOM.render(<div>Hello, world!</div>, document.getElementById('root'));
